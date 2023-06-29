4b47e2668ff870e5510e0601e11c8d9f47cff609
Shiraishi-Shodai <siranosuke1227@gmail.com>
2023-06-29 16:39:01 +0900

    写真表示まで

--- Test_DB/User.php
+++ Test_DB/User.php
@@ -7,16 +7,18 @@ class User
     private $user_name;
     private $affiliation_id;
     private $affiliation_name;
+    private $user_type_name;
     private $user_type_id;
     private $password;
 
-    public function __construct($user_id, $user_name, $affiliation_id, $affiliation_name, $user_type_id, $password)
+    public function __construct($user_id, $user_name, $affiliation_id, $affiliation_name, $user_type_name,  $user_type_id, $password)
     {
 
         $this->user_id = $user_id;
         $this->user_name = $user_name;
         $this->affiliation_id = $affiliation_id;
         $this->affiliation_name = $affiliation_name;
+        $this->user_type_name = $user_type_name;
         $this->user_type_id = $user_type_id;
         $this->password = $password;
     }
@@ -36,6 +38,10 @@ class User
     public function getAffiliationName(): string
     {
         return $this->affiliation_name;
+    }
+    public function getUserTypeName(): string
+    {
+        return $this->user_type_name;
     }
     public function getUserTypeId(): string
     {
